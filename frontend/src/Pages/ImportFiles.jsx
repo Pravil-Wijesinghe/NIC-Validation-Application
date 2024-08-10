8594d0d0d1da514f3ce2f034951a9b39c57a21b6
Pravil Wijesinghe <111410309+Pravil-Wijesinghe@users.noreply.github.com>
2024-08-10 09:07:33 +0530

    Create Services and connect to backend with User service

--- frontend/src/Pages/ImportFiles.jsx
+++ frontend/src/Pages/ImportFiles.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from '../Components/NavBar';
 import InputFileUpload from '../Components/InputFileUpload';
+import { Button } from '@mui/material';
 
 function ImportFiles() {
   return (
@@ -9,8 +10,9 @@ function ImportFiles() {
       <div className='relative flex items-center justify-center w-full h-screen font-montserrat bg-bg-color'>
         <div className='absolute items-center justify-center flex flex-col h-fit w-fit bg-primary rounded-xl text-black py-12 px-10'>
             <h1 className='font-bold text-3xl'>Import Files</h1>
-            <div className='mt-5'>
+            <div className='w-full flex flex-col justify-center mt-5 gap-4'>
                 <InputFileUpload/>
+                <Button variant='contained' className=''>Upload</Button>
             </div>
         </div>
     </div>
